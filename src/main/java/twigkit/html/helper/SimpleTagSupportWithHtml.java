04a7328a73a6d5859b1cea9f708761c3a06d6f03
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2012-03-07 14:52:59 +0000

    added tag helper

---
+++ src/main/java/twigkit/html/helper/SimpleTagSupportWithHtml.java
@@ -0,0 +1,133 @@
+package twigkit.html.helper;
+
+import twigkit.html.ContainerTag;
+import twigkit.html.SelfClosingTag;
+import twigkit.html.Tag;
+import twigkit.html.Text;
+import twigkit.html.attr.Attribute;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspTag;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * @author mr.olafsson
+ */
+public abstract class SimpleTagSupportWithHtml extends SimpleTagSupport {
+
+    private Writer writer;
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        setWriter(getJspContext().getOut());
+    }
+
+    protected void setWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    public ContainerTag div(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.DIV, attr);
+    }
+
+    public ContainerTag span(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.SPAN, attr);
+    }
+
+    public ContainerTag h1(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.H1, attr);
+    }
+
+    public ContainerTag h2(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.H2, attr);
+    }
+
+    public ContainerTag h3(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.H3, attr);
+    }
+
+    public ContainerTag h4(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.H4, attr);
+    }
+
+    public ContainerTag p(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.P, attr);
+    }
+
+    public ContainerTag ul(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.UL, attr);
+    }
+
+    public ContainerTag ol(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.OL, attr);
+    }
+
+    public ContainerTag li(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.LI, attr);
+    }
+
+    public ContainerTag dl(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.DL, attr);
+    }
+
+
+    public ContainerTag dt(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.DT, attr);
+    }
+
+    public ContainerTag dd(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.DD, attr);
+    }
+
+    public ContainerTag a(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.A, attr);
+    }
+
+    public SelfClosingTag img(Attribute... attr) throws IOException {
+        return new SelfClosingTag(writer, Tag.IMG, attr);
+    }
+
+    public ContainerTag form(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.FORM, attr);
+    }
+
+    public ContainerTag input(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.INPUT, attr);
+    }
+
+    public ContainerTag fieldset(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.FIELDSET, attr);
+    }
+
+    public ContainerTag legend(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.LEGEND, attr);
+    }
+
+    public ContainerTag script(Attribute... attr) throws IOException {
+        return new ContainerTag(writer, Tag.SCRIPT, attr);
+    }
+
+
+    public Text text(String text) throws IOException {
+        return new Text(writer, text);
+    }
+
+
+    public Attribute attr(String name, String... values) {
+        return new Attribute(name, values);
+    }
+
+    public Attribute.Css css(String... values) {
+        return new Attribute.Css(values);
+    }
+
+    public Attribute.Id id(String id) {
+        return new Attribute.Id(id);
+    }
+
+    public Attribute.Data data(String name, String value) {
+        return new Attribute.Data(name, value);
+    }
+}
